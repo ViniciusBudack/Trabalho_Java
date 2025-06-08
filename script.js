23a96e3ba82ba033d1c5f98e3003bd3fa8a35839
Danilo310705 <danilo310705@edu.unipar.br>
2025-06-08 19:51:45 -0300

    meu segundo exercicio
    fiz o meu segundo exercicio, adicionei mais uma imagem no primeiro, dei uma separada nos meus

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*Parte Do Danilo Super Bonitão👊😎🤏*/
+/*Inicio do calculo dos triangulos😎*/
 let xTriangulo = document.querySelector("#xTriangulo");
 let yTriangulo = document.querySelector("#yTriangulo");
 let zTriangulo = document.querySelector("#zTriangulo");
@@ -19,7 +21,7 @@ function calcularTipoDeTriangulo(){
         imgTriangulo.src = "img/trianguloEquilatero.jpeg";
     }if ((x === y && y != z || x != z) || (x === z && z!= y || x != y)){// Verifica se não é um triângulo Isósceles
         resultadoTriangulo.textContent = "Isósceles";
-        imgTriangulo.src = "img/.jpeg";
+        imgTriangulo.src = "img/trianguloIsoceles.png";
     }if ( x != y && z != y && x != z){// Verifica se não é um triângulo Escaleno
         resultadoTriangulo.textContent = "Escaleno";
         imgTriangulo.src = "img/.jpeg";
@@ -30,3 +32,73 @@ function calcularTipoDeTriangulo(){
 btCalcularTriangulo.onclick = function(){
     calcularTipoDeTriangulo();
 }
+/*final do calculo dos triangulos😎*/
+
+/*Inicio do calculo pra lanches😎*/
+let quantCachorroQuente = document.querySelector("#quantCachorroQuente");
+let quantBauru = document.querySelector("#quantBauru");
+let quantMistoQuente = document.querySelector("#quantMistoQuente");
+let quantHamburguer = document.querySelector("#quantHamburguer");
+let quantCheeseburguer = document.querySelector("#quantCheeseburguer");
+let quantRefrigerante = document.querySelector("#quantRefrigerante");
+let valorCachorroQuente = document.querySelector("#valorCachorroQuente");
+let valorBauru = document.querySelector("#valorBauru");
+let valorMistoQuente = document.querySelector("#valorMistoQuente");
+let valorHamburguer = document.querySelector("#valorHamburguer");
+let valorCheeseburguer = document.querySelector("#valorCheeseburguer");
+let valorRefrigerante = document.querySelector("#valorRefrigerante");
+let btCalcularLanches = document.querySelector("#btCalcularLanches");
+let totalDeLanche = document.querySelector("#totalDeLanche");
+
+function calcularLanches(){
+    /*na primeira parte esta sendo criado as variavei já com o valor total de cada lanche*/
+    let cachorroQuente = Number(quantCachorroQuente.value) * 11;
+    let bauru = Number(quantBauru.value) * 8.50;
+    let mistoQuente = Number(quantMistoQuente.value) * 8;
+    let hamburguer = Number(quantHamburguer.value) * 9;
+    let cheeseburguer = Number(quantCheeseburguer.value) * 10;
+    let refrigerante = Number(quantRefrigerante.value) * 4.50;
+    /*aqui é checado se o valor é maior que 0, para que n fique valores negativos*/
+    if(cachorroQuente >= 0){
+        valorCachorroQuente.textContent = "R$ " + cachorroQuente.toFixed(2);
+    }else{
+        valorCachorroQuente.textContent = "R$ " + 0;
+    }
+    if(bauru >= 0){
+        valorBauru.textContent = "R$ " + bauru.toFixed(2);
+    }else{
+        valorBauru.textContent = "R$ " + 0;
+    }
+    if(mistoQuente >= 0){
+        valorMistoQuente.textContent = "R$ " + mistoQuente.toFixed(2);
+    }else{
+        valorMistoQuente.textContent = "R$ " + 0;
+    }
+    if(hamburguer >= 0){
+        valorHamburguer.textContent = "R$ " + hamburguer.toFixed(2);
+    }else{
+        valorHamburguer.textContent = "R$ " + 0;
+    }
+    if(cheeseburguer >= 0){
+        valorCheeseburguer.textContent = "R$ " + cheeseburguer.toFixed(2);
+    }else{
+        valorCheeseburguer.textContent = "R$ " + 0;
+    }
+    if(refrigerante >= 0){
+        valorRefrigerante.textContent = "R$ " + refrigerante.toFixed(2);
+    }else{
+        valorRefrigerante.textContent = "R$ " + 0;
+    }
+    /*soma total de todos os lanches */
+    totalDeLanche.textContent = "R$ " + (cachorroQuente + bauru + mistoQuente + hamburguer + cheeseburguer + refrigerante).toFixed(2);
+}
+
+btCalcularLanches.onclick = function(){
+    calcularLanches();
+}
+/*final do calculo pra lanches😎*/
+
+/*Inicio do Sistema de pagamentos😎*/
+/*final do Sistema de pagamentos😎*/
+
+/* Fim da Parte Do Danilo Super Bonitão👊😎🤏*/
